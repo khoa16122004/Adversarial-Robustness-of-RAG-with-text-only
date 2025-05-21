b101082c037854dfa8993e21cd0a09b575cef933
khoa16122004 <khoatr.work@gmail.com>
2025-05-21 22:18:24 +0700

    Update reader.py

--- reader.py
+++ reader.py
@@ -154,7 +154,7 @@ if __name__ == "__main__":
         score = reader(question, contexts, ans)
         scores.append(score)
     
-    score_normalize = scores / sum(scores)
+    score_normalize = np.array(scores) / sum(scores)
     print("Score: ", score_normalize)
         
     
