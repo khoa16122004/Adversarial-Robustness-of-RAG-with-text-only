07f256e7f89dd2400b30fa3ea1a1317a7b8d9c86
khoa16122004 <khoatr.work@gmail.com>
2025-05-23 13:28:05 +0700

    Update fitness.py

--- fitness.py
+++ fitness.py
@@ -90,7 +90,7 @@ if __name__ == "__main__":
                           0.5, 0.5,
                           question, original_text, answer) 
     
-    dual_scores = fitness(question, contexts, answer)
+    # dual_scores = fitness(question, contexts, answer)
     # print(dual_scores)
     
     for context in contexts:
