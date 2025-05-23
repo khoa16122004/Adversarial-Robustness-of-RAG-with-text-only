2db8bb12a98195f71909d49b3000e094d32e7136
khoa16122004 <khoatr.work@gmail.com>
2025-05-23 13:40:36 +0700

    Update reader.py

--- reader.py
+++ reader.py
@@ -53,7 +53,7 @@ class Reader(torch.nn.Module):
     def forward(self, question, contexts, answer): # logits scores
         inputs = [self.template.format(q=question, d=text) for text in contexts]
         labels = [answer] * len(inputs)
-        
+        print("Contexts: ", contexts)
         print("Len inputs: ", len(inputs))
         
         input_embeddings = self.tokenizer(
