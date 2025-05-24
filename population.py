22d7a2a20b4bbbae6c39fa879a5a4eca5f2f3bf6
khoa16122004 <khoatr.work@gmail.com>
2025-05-24 23:41:18 +0700

    Update population.py

--- population.py
+++ population.py
@@ -105,7 +105,9 @@ class Population:
             if random.random() < mutation_prob:
                 new_idx = random.choice([i for i in range(len(self.original_splits)) if i not in ind.position_answer and i not in indices])
                 print("New_index: ", new_idx)
-                new_words = self.transformation.get_replacement_words(self.original_splits[new_idx])
+                new_words = None
+                while not new_words:
+                    new_words = self.transformation.get_replacement_words(self.original_splits[new_idx])
                 mutate_idx = random.choice(indices)
                 pos_idx = indices.index(mutate_idx)
                 indices.pop(pos_idx)
