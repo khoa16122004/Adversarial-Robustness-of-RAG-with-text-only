b631bd25e33b6e76c848fe2266c67c35f023ded5
khoa16122004 <khoatr.work@gmail.com>
2025-05-26 14:03:34 +0700

    Update test.py

--- test.py
+++ test.py
@@ -12,7 +12,17 @@ def find_anser(context, answer):
 
 context = "The Great Wall of China is one of the most iconic structures in the world, built over many centuries by various Chinese dynasties. The construction of this grand wall began in the 7th century BC and continued through several dynasties, notably the Ming Dynasty. Stretching an impressive length of approximately 13,000 miles, it runs across northern China and has become a significant symbol of Chinese history and culture."
 context_split = context.split()
-print(context_split[51])
 answer = "13,000 miles"
 
 # print(find_anser(context, answer))
+
+from population import Individual
+modified_indecies = [38, 7, 45, 13, 16, 63, 32, 33, 36, 12, 20, 52, 27, 21, 6, 17, 47, 66, 58]
+words = ["Khoa"] * len(modified_indecies)
+ind = Individual(
+    context, 
+    answer, 
+    replacement_words=words, 
+    modified_indices=modified_indecies
+)
+print(ind.get_perturbed_text())
