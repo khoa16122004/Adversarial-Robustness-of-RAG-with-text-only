171ff7e35060eadc14dcdc1e71f5466f92fd4601
khoa16122004 <khoatr.work@gmail.com>
2025-05-26 13:57:10 +0700

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,8 @@ def find_anser(context, answer):
     return results
 
 context = "The Great Wall of China is one of the most iconic structures in the world, built over many centuries by various Chinese dynasties. The construction of this grand wall began in the 7th century BC and continued through several dynasties, notably the Ming Dynasty. Stretching an impressive length of approximately 13,000 miles, it runs across northern China and has become a significant symbol of Chinese history and culture."
-
+context_split = context.split()
+print(context_split[51])
 answer = "13,000 miles"
 
 print(find_anser(context, answer))
