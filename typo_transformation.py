e47dbe93f291644b23158cbe24d9c9d8c7870923
khoa16122004 <khoatr.work@gmail.com>
2025-05-26 14:34:31 +0700

    Update typo_transformation.py

--- typo_transformation.py
+++ typo_transformation.py
@@ -19,7 +19,7 @@ class BaseTypoTransformation:
                 self.typos[line[0]] = line[1:]
         if os.path.exists(key_path):
             for line in open(key_path):
-                line = line.split()
+                line = split(line)
                 self.NN[line[0]] = line[1:]
 
     def get_replacement_words(self, word):
