f4dacf6e1befe0170de2a902c747e4e87e7a9e15
khoa16122004 <khoatr.work@gmail.com>
2025-05-30 14:38:33 +0700

    Update reader.py

--- reader.py
+++ reader.py
@@ -208,7 +208,7 @@ if __name__ == "__main__":
     reader = Reader(model_name="Llama-7b")
 
     question = "What significant event did the James Webb Space Telescope achieve on December 25, 2021, that enhances our understanding of the universe?"
-    adv_contexts = ['The a b Space Telescope (JWST), a marvel of modern engineering, s successfully launched into space on December 5, 2021. This astronomical instrument is currently orbiting the Sun at the second Lagrange point (L2), a location that allows it to maintain a stable position r to the Earth and Sun. With its exceptional infrared capabilities, the JWST is designed to observe ancient galaxies, providing i into the early s and expanding our understanding of cosmic history. Its deployment m a significant milestone in space exploration and astronomy.',
+    adv_contexts = ['The s Webb e e (T), w j o f g, a successfully c o speace on r 2, 2021. T m i is n i e C w h s a k (2), z a h a it to maintain s s n v g t E e u. i its exceptional i e, the JWST n n u observe e a, providing i t E a r and a r t i o history. I n s a significative n n e p e t.',
                     ]
 
     context = "The James Webb Space Telescope (JWST), a marvel of modern engineering, was successfully launched into space on December 25, 2021. This astronomical instrument is currently orbiting the Sun at the second Lagrange point (L2), a location that allows it to maintain a stable position relative to the Earth and Sun. With its exceptional infrared capabilities, the JWST is designed to observe ancient galaxies, providing insights into the early universe and expanding our understanding of cosmic history. Its deployment marks a significant milestone in space exploration and astronomy."
