79a1ab8edae1729acda5538ad4a4e97afdc0b663
khoa16122004 <khoatr.work@gmail.com>
2025-05-30 08:55:15 +0700

    Update reader.py

--- reader.py
+++ reader.py
@@ -297,6 +297,7 @@ if __name__ == "__main__":
     reader = Reader(model_name="Llama-7b")
 
     question = "What significant event did the James Webb Space Telescope achieve on December 25, 2021, that enhances our understanding of the universe?"
-    context =  "The James Webb Space Telescope (JWST), a marvel of modern engineering, was successfully launched knto space 9n December 25, 2021. This astronomical instrument is currently orbiting the Sun at the second Lagrznge point (L2), a location that allows in to maintain a stable positlon relatice to the Earth and Sun. With its exceptional infrared capabilities, the JWST is designed to observe ancient galaxies, providing insights into the early universe and expanding our understanding of cksmic histort. Its deployment marks a significant milestone in space exploration and astronomy."
+    context =  "The James Webb Space Telescope (JWST), a marvel of modern engineering, was successfully launched into space on December 25, 2021. This astronomical instrument is currently orbiting the Sun at the second Lagrange point (L2), a location that allows it to maintain a stable position relative to the Earth and Sun. With its exceptional infrared capabilities, the JWST is designed to observe ancient galaxies, providing insights into the early universe and expanding our understanding of cosmic history. Its deployment marks a significant milestone in space exploration and astronomy."
+
     output = reader.generate(question, [context])
     print(output)
