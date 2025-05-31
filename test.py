8777b5d52626300324f4fdf7c4409a88e52b8af0
khoa16122004 <khoatr.work@gmail.com>
2025-05-31 13:34:05 +0700

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from utils import set_seed_everything, DataLoader
 import json
 dataset = DataLoader("sample5_data.json")
 
-for i in range(len(dataset)):
+for i in range(dataset.len()):
     original_text, question, gt_answer = dataset.take_sample(i)
     split_ = split(original_text)
     position = int(input())
