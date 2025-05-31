9690f2bf3ff005c2cc63cf2b152c9bf2b1049d08
khoa16122004 <khoatr.work@gmail.com>
2025-05-31 08:49:50 +0700

    update

---
+++ evaluate.py
@@ -0,0 +1,33 @@
+import argparse
+from algorithm import GA, NSGAII
+from population import  Population, Individual
+from fitness import WeightedSUm, MultiScore, Targeted_MultiScore, Targeted_WeightedSUm
+import numpy as np
+from utils import set_seed_everything, DataLoader
+from reader import Reader
+from utils import find_answer, split
+
+def main():
+    set_seed_everything(22520691)
+    dataset = DataLoader(args.data_path)
+    len_dataset = dataset.len()
+    
+    reader = Reader(args.reader_name)
+    with open(args.adv_text_path, "r") as f:
+        adv_texts = [line.strip() for line in f.readlines()]
+        
+    original_text, question, gt_answer = dataset.take_sample(args.evaluate_id)
+    inference_text = adv_texts + [original_text]
+    output = reader.generate(question, inference_text)
+    print("Output: ", output)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run GA attack")
+    parser.add_argument("--data_path", type=str)
+    parser.add_argument("--reader_name", default="Llama-7b", type=str, help="Retriever model name")
+    parser.add_argument("--q_name", default="facebook/dpr-question_encoder-multiset-base", type=str, help="Question encoder name")
+    parser.add_argument("--c_name", default="facebook/dpr-ctx_encoder-multiset-base", type=str, help="Context encoder name")
+    parser.add_argument("--data_path", default="sample5_data.json", type=str, help="Data path")
+    parser.add_argument("--evaluate_id", default=0, type=int, help="Evaluate id")
+    parser.add_argument("adv_text_path", type=str)
+    args = parser.parse_args()
---
+++ run.sh
@@ -0,0 +1,4 @@
+python run.py --data_path sample5_data.json --n_iter 500 --pop_size 50 --algorithm NSGAII --pct_words_to_swap 0.1
+python run.py --data_path sample5_data.json --n_iter 500 --pop_size 50 --algorithm NSGAII --pct_words_to_swap 0.2
+python run.py --data_path sample5_data.json --n_iter 500 --pop_size 50 --algorithm GA --pct_words_to_swap 0.1
+python run.py --data_path sample5_data.json --n_iter 500 --pop_size 50 --algorithm GA --pct_words_to_swap 0.2
