658aff28c43f4189da62f380315f55538e178dba
khoa16122004 <khoatr.work@gmail.com>
2025-05-31 18:23:33 +0700

    Update evaluate_jerry_pick.py

--- evaluate_jerry_pick.py
+++ evaluate_jerry_pick.py
@@ -17,14 +17,8 @@ def main(args):
     for i in range(len_dataset):
         original_text, question, gt_answer = dataset.take_sample(i)
         golden_answer = reader.generate(question, [original_text])[0]
-        fitness = MultiScore(args.reader_name, 
-                args.q_name, 
-                args.c_name, 
-                question, original_text, golden_answer)
-        score = fitness(question, [original_text], golden_answer)
         print("Golden answer: ", golden_answer)
         print("Gt answer: ", gt_answer)
-        print("score")
 
 
 if __name__ == "__main__":
