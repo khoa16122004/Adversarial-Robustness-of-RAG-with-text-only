4b4da811a02cea805a93658db362366216781100
khoa16122004 <khoatr.work@gmail.com>
2025-05-31 13:35:05 +0700

    Update test.py

--- test.py
+++ test.py
@@ -6,5 +6,7 @@ dataset = DataLoader("sample5_data.json")
 for i in range(dataset.len()):
     original_text, question, gt_answer = dataset.take_sample(i)
     split_ = split(original_text)
+    print("Split: ", split_)
+    print("Answer: ", gt_answer)
     position = int(input())
     print(split_[position])
