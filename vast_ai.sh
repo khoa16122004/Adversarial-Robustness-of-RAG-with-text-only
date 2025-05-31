fb281b5d5dc2581e5b38fd55fb16f9ed11ac6189
khoa16122004 <khoatr.work@gmail.com>
2025-05-31 07:36:30 +0700

    Update vast_ai.sh

--- vast_ai.sh
+++ vast_ai.sh
@@ -6,8 +6,6 @@ sudo apt update && sudo apt upgrade -y
 # Cài đặt screen
 sudo apt install screen -y
 
-# Clone repository
-git clone https://github.com/khoa16122004/EnhanceGARAG
 
 # Tạo file requirements.txt
 cat <<EOF > requirements.txt
