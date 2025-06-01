7dece1a64de3a68639daf8c0f00828decdff8865
khoa16122004 <khoatr.work@gmail.com>
2025-06-01 23:02:59 +0700

    update

--- run_01_005.sh
+++ run_01_005.sh
@@ -1,3 +1,3 @@
-python run.py --data_path data_new_v2.json --n_iter 100 --pop_size 50 --algorithm NSGAII --pct_words_to_swap 0.1 --start_idx 0 --end_idx 10 --reader_name vicuna-7b 
-python run.py --data_path data_new_v2.json --n_iter 100 --pop_size 50 --algorithm NSGAII --pct_words_to_swap 0.05 --start_idx 0 --end_idx 10 --reader_name vicuna-7b
+CUDA_VISIBLE_DEVICES=7 python run.py --data_path data_new_v2.json --n_iter 100 --pop_size 50 --algorithm NSGAII --pct_words_to_swap 0.1 --start_idx 0 --end_idx 10 --reader_name vicuna-7b 
+CUDA_VISIBLE_DEVICES=7 python run.py --data_path data_new_v2.json --n_iter 100 --pop_size 50 --algorithm NSGAII --pct_words_to_swap 0.05 --start_idx 0 --end_idx 10 --reader_name vicuna-7b
 
