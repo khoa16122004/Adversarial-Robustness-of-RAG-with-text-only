de70dd40a4b672311687dc5a35b65432db1267af
khoa16122004 <khoatr.work@gmail.com>
2025-06-01 07:05:39 +0700

    update

--- run.sh
+++
@@ -1,6 +0,0 @@
-python run.py --data_path data_new_v2.json --n_iter 100 --pop_size 50 --algorithm NSGAII --pct_words_to_swap 0.1 --start_idx 0 --end_idx 10
-python run.py --data_path data_new_v2.json --n_iter 100 --pop_size 50 --algorithm NSGAII --pct_words_to_swap 0.05 --start_idx 0 --end_idx 10
-python run.py --data_path data_new_v2.json --n_iter 100 --pop_size 50 --algorithm NSGAII --pct_words_to_swap 0.2 --start_idx 0 --end_idx 10
-python run.py --data_path data_new_v2.json --n_iter 100 --pop_size 50 --algorithm NSGAII --pct_words_to_swap 0.1 --start_idx 0 --end_idx 10 --fitness_statery target_answer
-python run.py --data_path data_new_v2.json --n_iter 100 --pop_size 50 --algorithm NGSAII --pct_words_to_swap 0.05 --start_idx 0 --end_idx 10 --fitness_statery target_answer
-python run.py --data_path data_new_v2.json --n_iter 100 --pop_size 50 --algorithm NGSAII --pct_words_to_swap 0.2 --start_idx 0 --end_idx 10 --fitness_statery target_answer
---
+++ run_02_05.sh
@@ -0,0 +1,2 @@
+python run.py --data_path data_new_v2.json --n_iter 100 --pop_size 50 --algorithm NSGAII --pct_words_to_swap 0.2 --start_idx 0 --end_idx 10
+python run.py --data_path data_new_v2.json --n_iter 100 --pop_size 50 --algorithm NSGAII --pct_words_to_swap 0.5 --start_idx 0 --end_idx 10
