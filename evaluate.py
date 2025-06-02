fda43378f095b45f74e23c0f65caeadb4461ee82
khoa16122004 <khoatr.work@gmail.com>
2025-06-02 21:22:46 +0700

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,13 +1,14 @@
 import argparse
 from algorithm import GA, NSGAII
-from population import  Population, Individual
+from population import Population, Individual
 from fitness import WeightedSUm, MultiScore, Targeted_MultiScore, Targeted_WeightedSUm
 import numpy as np
 from utils import set_seed_everything, DataLoader
 from reader import Reader
 from utils import find_answer, split
-from fitness import MultiScore
 import os
+import json
+
 def main(args):
     set_seed_everything(22520691)
     dataset = DataLoader(args.data_path)
@@ -19,19 +20,23 @@ def main(args):
         print("=======================================")
         print("PCT: ", pct)
         dir_ = f"pertubed_text/{args.reader_name}_{pct}"
+        output_path = f"output_{args.reader_name}_{pct}.json"
+        results = []
+
         for i in range(len_dataset):
             path = os.path.join(dir_, f"{i}.txt")
             original_text, question, gt_answer, _ = dataset.take_sample(i)
             adv_text = open(path, "r").readline().strip()
             contexts = [original_text, adv_text]
             output = reader.generate(question, contexts)
+
             print("GT Answer: ", gt_answer)
             print("Output: ", output)
             
             fitness = MultiScore(
                 reader_name=args.reader_name,
-                q_name="facebook/dpr-question_encoder-multiset-base",
-                c_name="facebook/dpr-ctx_encoder-multiset-base",
+                q_name=args.q_name,
+                c_name=args.c_name,
                 question=question,
                 original_text=original_text,
                 answer=gt_answer
@@ -39,11 +44,19 @@ def main(args):
             
             score = fitness(question, [adv_text], gt_answer)
             print("Score: ", score)
-            
-                        
 
-    
+            result = {
+                "question": question,
+                "gt_answer": gt_answer,
+                "original_context": original_text,
+                "adv_context": adv_text,
+                "adv_output": output,
+                "score": score
+            }
+            results.append(result)
 
+        with open(output_path, "w", encoding="utf-8") as f:
+            json.dump(results, f, ensure_ascii=False, indent=2)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run GA attack")
